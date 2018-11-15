d93dc790d6b25cc51bcd36c114f293dccf5692c0
icesphere <icesphere12@gmail.com>
2018-11-15 06:19:19 -0700

    fix deck for dark ages cards

--- src/main/kotlin/com/kingdom/model/cards/darkages/DarkAgesCard.kt
+++ src/main/kotlin/com/kingdom/model/cards/darkages/DarkAgesCard.kt
@@ -4,4 +4,4 @@ import com.kingdom.model.cards.Card
 import com.kingdom.model.cards.CardType
 import com.kingdom.model.cards.Deck
 
-abstract class DarkAgesCard(name: String, type: CardType, cost: Int) : Card(name, Deck.Cornucopia, type, cost)
+abstract class DarkAgesCard(name: String, type: CardType, cost: Int) : Card(name, Deck.DarkAges, type, cost)
