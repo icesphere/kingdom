96c9d8fae704ae105bb5dbce719714955658d71c
icesphere <icesphere12@gmail.com>
2019-01-05 13:16:03 -0700

    Scouting Party

---
+++ src/main/kotlin/com/kingdom/model/cards/adventures/events/ScoutingParty.kt
@@ -0,0 +1,49 @@
+package com.kingdom.model.cards.adventures.events
+
+import com.kingdom.model.cards.Card
+import com.kingdom.model.cards.actions.ChooseCardsActionCard
+import com.kingdom.model.players.Player
+import com.kingdom.util.groupedString
+
+class ScoutingParty : AdventuresEvent(NAME, 2), ChooseCardsActionCard {
+
+    init {
+        addBuys = 1
+        special = "Look at the top 5 cards of your deck. Discard 3 and put the rest back in any order."
+        fontSize = 10
+    }
+
+    override fun cardPlayedSpecialAction(player: Player) {
+        val cards = player.removeTopCardsOfDeck(5)
+        if (cards.isEmpty()) {
+            return
+        }
+
+        if (cards.size <= 3) {
+            player.addCardsToDiscard(cards)
+            player.addEventLogWithUsername("Discarded ${cards.groupedString} from the top of their deck")
+        } else {
+
+        }
+        player.chooseCardsAction(3, "Discard 3 cards", this, cards, false, cards)
+    }
+
+    override fun onCardsChosen(player: Player, cards: List<Card>, info: Any?) {
+        @Suppress("UNCHECKED_CAST")
+        val topDeckCards = info as List<Card>
+
+        val cardsToPutBackOnDeck = topDeckCards - cards
+
+        player.addCardsToDiscard(cards, true)
+
+        if (cardsToPutBackOnDeck.size == 1) {
+            player.addCardToTopOfDeck(cardsToPutBackOnDeck.first())
+        } else {
+            player.putCardsOnTopOfDeckInAnyOrder(cardsToPutBackOnDeck)
+        }
+    }
+
+    companion object {
+        const val NAME: String = "Scouting Party"
+    }
+}
--- src/main/kotlin/com/kingdom/repository/CardRepository.kt
+++ src/main/kotlin/com/kingdom/repository/CardRepository.kt
@@ -4,10 +4,7 @@ import com.kingdom.model.cards.Card
 import com.kingdom.model.cards.Deck
 import com.kingdom.model.cards.Event
 import com.kingdom.model.cards.adventures.*
-import com.kingdom.model.cards.adventures.events.Alms
-import com.kingdom.model.cards.adventures.events.Borrow
-import com.kingdom.model.cards.adventures.events.Quest
-import com.kingdom.model.cards.adventures.events.Save
+import com.kingdom.model.cards.adventures.events.*
 import com.kingdom.model.cards.cornucopia.*
 import com.kingdom.model.cards.darkages.*
 import com.kingdom.model.cards.darkages.ruins.*
@@ -284,7 +281,8 @@ class CardRepository {
                 Alms(),
                 Borrow(),
                 Quest(),
-                Save()
+                Save(),
+                ScoutingParty()
         )
 
     val allCards: List<Card>
