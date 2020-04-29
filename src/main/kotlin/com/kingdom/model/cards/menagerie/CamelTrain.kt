2224646a9fb712ae10cecf8e1c27eab2c0d21dcf
icesphere <icesphere12@gmail.com>
2020-04-28 20:27:37 -0600

    Camel Train

--- src/main/kotlin/com/kingdom/model/cards/menagerie/CamelTrain.kt
+++ src/main/kotlin/com/kingdom/model/cards/menagerie/CamelTrain.kt
@@ -3,12 +3,14 @@ package com.kingdom.model.cards.menagerie
 import com.kingdom.model.cards.Card
 import com.kingdom.model.cards.CardType
 import com.kingdom.model.cards.actions.ChooseCardActionCard
+import com.kingdom.model.cards.listeners.AfterCardGainedListenerForSelf
+import com.kingdom.model.cards.supply.Gold
 import com.kingdom.model.players.Player
 
-class CamelTrain : MenagerieCard(NAME, CardType.Action, 3), ChooseCardActionCard, UsesExileMat {
+class CamelTrain : MenagerieCard(NAME, CardType.Action, 3), ChooseCardActionCard, UsesExileMat, AfterCardGainedListenerForSelf {
 
     init {
-        special = "Exile a non-Victory card from the Supply."
+        special = "Exile a non-Victory card from the Supply. When you gain this, Exile a Gold from the Supply."
     }
 
     override fun cardPlayedSpecialAction(player: Player) {
@@ -17,6 +19,10 @@ class CamelTrain : MenagerieCard(NAME, CardType.Action, 3), ChooseCardActionCard
 
     override fun onCardChosen(player: Player, card: Card, info: Any?) {
         player.exileCardFromHand(card)
+    }
+
+    override fun afterCardGained(player: Player) {
+        player.exileCardFromSupply(Gold())
     }
 
     companion object {
