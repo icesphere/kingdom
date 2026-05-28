d70c6f753edb979aeced9a6a702d23e6198e840f
johncazier <johncazier@gmail.com>
2026-05-27 21:39:39 -0600

    rename card

--- src/main/kotlin/com/kingdom/model/cards/intrigue/Farm.kt
+++ src/main/kotlin/com/kingdom/model/cards/intrigue/Farm.kt
@@ -2,16 +2,15 @@ package com.kingdom.model.cards.intrigue
 
 import com.kingdom.model.cards.CardType
 
-class TreasureRoom : IntrigueCard(NAME, CardType.TreasureVictory, 6) {
+class Farm : IntrigueCard(NAME, CardType.TreasureVictory, 6) {
 
     init {
         addCoins = 2
         victoryPoints = 2
-        fontSize = 9
     }
 
     companion object {
-        const val NAME: String = "Treasure Room"
+        const val NAME: String = "Farm"
     }
 }
 
